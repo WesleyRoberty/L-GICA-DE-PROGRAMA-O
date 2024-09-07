979d51f817710313928d2aaa5675f104cc85eebe
Wesley Roberty <wesley_roberty@hotmail.com>
2024-09-06 21:09:47 -0300

    Add files via upload

---
+++ AULA 1/parseint.js
@@ -0,0 +1,15 @@
+/*let vinte = Number(prompt("Digite 20 e pressione Enter: "));
+document.write(vinte +1);
+
+let nome = "Maria Eduarda";
+let idade = 20;
+console.log(nome ," tem ",idade," anos.");
+
+let nome = "Geovana";
+let idade = 21;
+alert(nome+" tem "+idade+" anos.");*/
+
+let anoAtual = 2024;
+let nascimento = 1998;
+let nome = "Wesley";
+alert(`${nome} nasceu em ${nascimento}, Se estamos em ${anoAtual}, ele certamente tem ${anoAtual- nascimento} anos`);
---
+++ AULA 1/tipagem.py
@@ -0,0 +1,5 @@
+nome= "Wesley Roberty"; #str
+idade = 26; #int
+print(nome+ " " + idade); #typeError: can  only concatenate str (not "int) to str
+
+
